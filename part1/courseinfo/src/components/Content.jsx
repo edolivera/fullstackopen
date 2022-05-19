cb4b30a34bb0ce0657c5c03b5395cc92d52b9392
edolivera <contatodoed@hotmail.com>
2022-05-19 12:00:31 -0300

    Exercise1.2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
+import Part1 from './Part1'
+import Part2 from './Part2'
+import Part3 from './Part3'
 
 const Content = (props) => {
   return (
     <div>
-        <p>
-            {props.part1} {props.exercises1}
-        </p>
-        <p>
-            {props.part2} {props.exercises2}
-        </p>
-        <p>
-            {props.part3} {props.exercises3}
-        </p>
+      <Part1 part1={props.part1} exercises1={props.exercises1} />
+      <Part2 part2={props.part2} exercises1={props.exercises2} />
+      <Part3 part3={props.part3} exercises3={props.exercises3} />
     </div>
   )
 }
---
+++ part1/courseinfo/src/components/Part3.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Part3 = (props) => {
+  return (
+    <div>
+        <p>
+            {props.part3} {props.exercises3}
+        </p>
+    </div>
+  )
+}
+
+export default Part3
