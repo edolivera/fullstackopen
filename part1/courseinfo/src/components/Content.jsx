773858336dbdd21df751d2f6d07b3976e1015661
edolivera <contatodoed@hotmail.com>
2022-05-19 17:37:50 -0300

    Exercise1.4

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -4,12 +4,12 @@ import Part2 from './Part2'
 import Part3 from './Part3'
 
 const Content = (props) => {
-  
+
   return (
     <div>
-      <Part1 part1={props.part1}  />
-      <Part2 part2={props.part2} />
-      <Part3 part3={props.part3} />
+      <Part1 part1={props.parts[0]}  />
+      <Part2 part2={props.parts[1]} />
+      <Part3 part3={props.parts[2]} />
     </div>
   )
 }
